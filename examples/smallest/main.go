c2505f3d33c4ab7be3376484f802b72fc385ad25
yehoyada <yehoyada@getbrew.com>
2023-02-19 22:03:42 +0200

    add example

---
+++ examples/smallest/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/hvuhsg/goapi"
+
+func main() {
+	app := goapi.GoAPI("small", "1.0v")
+
+	echo := app.Path("/echo")
+	echo.Methods(goapi.GET)
+	echo.Description("echo a back")
+	echo.Parameter("a", goapi.QUERY, goapi.VRequired{}, goapi.VIsInt{})
+	echo.Action(func(request *goapi.Request) goapi.Response {
+		return goapi.JsonResponse{"a": request.GetInt("a")}
+	})
+
+	app.Run("127.0.0.1", 8080)
+}
