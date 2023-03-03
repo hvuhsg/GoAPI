4cc15ccdf98d3af50766fce273b11de81448c0c9
yehoyada <yehoyada@getbrew.com>
2023-03-03 12:43:46 +0200

    add validatos

--- validator.go
+++ validator.go
@@ -1,7 +1,13 @@
 package goapi
 
 import (
+	"errors"
 	"fmt"
+	"net"
+	"net/url"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/getkin/kin-openapi/openapi3"
 )
@@ -190,6 +196,210 @@ func (v VRange) Validate(r *Request, paramName string) error {
 
 	if fVal < v.Min || fVal > v.Max {
 		return fmt.Errorf("parameter %s must be between %f and %f", paramName, v.Min, v.Max)
+	}
+
+	return nil
+}
+
+type VIsEmail struct{}
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+
+func (v VIsEmail) updateOpenAPISchema(schema *openapi3.Schema) {
+	schema.Format = "email"
+}
+func (v VIsEmail) Validate(r *Request, paramName string) error {
+	email := r.GetString(paramName)
+	if !emailRegex.MatchString(email) {
+		return fmt.Errorf("parameter %s must be a valid email address", paramName)
+	}
+	return nil
+}
+
+type VIsURL struct{}
+
+func (v VIsURL) updateOpenAPISchema(schema *openapi3.Schema) {
+	schema.Format = "url"
+}
+func (v VIsURL) Validate(r *Request, paramName string) error {
+	urlStr := r.GetString(paramName)
+
+	// Parse the URL and check for any errors
+	_, err := url.Parse(urlStr)
+	if err != nil {
+		return fmt.Errorf("parameter %s must be a valid URL: %v", paramName, err)
+	}
+
+	return nil
+}
+
+type VIsUUID struct{}
+
+var uuidRegex = regexp.MustCompile(`^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`)
+
+func (v VIsUUID) updateOpenAPISchema(schema *openapi3.Schema) {
+	schema.Format = "uuid"
+}
+func (v VIsUUID) Validate(r *Request, paramName string) error {
+	uuidStr := r.GetString(paramName)
+
+	// Check if the UUID string matches the expected format
+	if !uuidRegex.MatchString(uuidStr) {
+		return fmt.Errorf("parameter %s must be a valid UUID", paramName)
+	}
+
+	// Convert the UUID string to lowercase and check for any non-hexadecimal characters
+	uuidStr = strings.ReplaceAll(strings.ToLower(uuidStr), "-", "")
+	if _, err := fmt.Sscanf(uuidStr, "%x", &struct{}{}); err != nil {
+		return fmt.Errorf("parameter %s must be a valid UUID", paramName)
+	}
+
+	return nil
+}
+
+type VIsDate struct {
+	// The expected date format. This should be a string that specifies the expected format of the date, using the following standard Go date and time format codes: https://golang.org/pkg/time/#pkg-constants.
+	// For example, to specify a date in the format "YYYY-MM-DD", you can set Format to "2006-01-02".
+	Format string
+}
+
+func (v VIsDate) updateOpenAPISchema(schema *openapi3.Schema) {
+	schema.Format = "date"
+}
+func (v VIsDate) Validate(r *Request, paramName string) error {
+	dateStr := r.GetString(paramName)
+
+	// Parse the date string based on the specified format
+	parsedDate, err := time.Parse(v.Format, dateStr)
+	if err != nil {
+		return fmt.Errorf("parameter %s must be a valid date in the format %s", paramName, v.Format)
+	}
+
+	// Check if the parsed date is valid
+	if parsedDate.IsZero() {
+		return fmt.Errorf("parameter %s must be a valid date in the format %s", paramName, v.Format)
+	}
+
+	return nil
+}
+
+type VRegex struct {
+	Regex string
+}
+
+func (v VRegex) updateOpenAPISchema(schema *openapi3.Schema) {
+	// Not applicable
+}
+
+func (v VRegex) Validate(r *Request, paramName string) error {
+	str := r.GetString(paramName)
+
+	// Compile the regular expression
+	re, err := regexp.Compile(v.Regex)
+	if err != nil {
+		return fmt.Errorf("failed to compile regular expression: %v", err)
+	}
+
+	// Match the string against the regular expression
+	if !re.MatchString(str) {
+		return fmt.Errorf("parameter %s must match the regular expression %s", paramName, v.Regex)
+	}
+
+	return nil
+}
+
+type VIsTime struct {
+	// The expected format of the time string. This should be a string that specifies the expected format of the time, using the following standard Go date and time format codes: https://golang.org/pkg/time/#pkg-constants.
+	// For example, to specify a time in the format "15:04:05", you can set Format to "15:04:05".
+	Format string
+}
+
+func (v VIsTime) updateOpenAPISchema(schema *openapi3.Schema) {
+	schema.Format = "time"
+}
+
+func (v VIsTime) Validate(r *Request, paramName string) error {
+	timeStr := r.GetString(paramName)
+
+	// Parse the time string based on the specified format
+	parsedTime, err := time.Parse(v.Format, timeStr)
+	if err != nil {
+		return fmt.Errorf("parameter %s must be a valid time in the format %s", paramName, v.Format)
+	}
+
+	// Check if the parsed time is valid
+	if parsedTime.IsZero() {
+		return fmt.Errorf("parameter %s must be a valid time in the format %s", paramName, v.Format)
+	}
+
+	return nil
+}
+
+type VPassword struct {
+	MinLength      int
+	MaxLength      int
+	RequireSymbols bool
+	RequireNumbers bool
+	RequireUpper   bool
+}
+
+func (v VPassword) updateOpenAPISchema(schema *openapi3.Schema) {
+	// Not applicable
+}
+
+func (v VPassword) Validate(r *Request, paramName string) error {
+	password := r.GetString(paramName)
+
+	// Check length requirements
+	if len(password) < v.MinLength || len(password) > v.MaxLength {
+		return fmt.Errorf("parameter %s must have length between %d and %d", paramName, v.MinLength, v.MaxLength)
+	}
+
+	// Check symbol requirements
+	if v.RequireSymbols && !regexp.MustCompile(`[!@#\$%\^&\*\(\)_\+\{\}\[\]:;<>\?,\.\|\\\/~]`).MatchString(password) {
+		return errors.New("password must contain at least one symbol")
+	}
+
+	// Check number requirements
+	if v.RequireNumbers && !regexp.MustCompile(`[0-9]`).MatchString(password) {
+		return errors.New("password must contain at least one number")
+	}
+
+	// Check uppercase letter requirements
+	if v.RequireUpper && !regexp.MustCompile(`[A-Z]`).MatchString(password) {
+		return errors.New("password must contain at least one uppercase letter")
+	}
+
+	return nil
+}
+
+type VIPAddress struct{}
+
+func (v VIPAddress) Validate(r *Request, paramName string) error {
+	ipStr := r.GetString(paramName)
+
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		return fmt.Errorf("parameter %s must be a valid IP address", paramName)
+	}
+
+	return nil
+}
+
+type VPhoneNumber struct {
+	Prefix string
+}
+
+func (v VPhoneNumber) Validate(r *Request, paramName string) error {
+	phoneNumber := r.GetString(paramName)
+
+	// Regular expression for phone numbers with optional prefix and extension
+	// The prefix is specified in the validator
+	// Source: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s02.html
+	re := regexp.MustCompile(fmt.Sprintf(`^%s?\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}(?: *x\d+)?$`, v.Prefix))
+
+	if !re.MatchString(phoneNumber) {
+		return fmt.Errorf("parameter %s must be a valid phone number with prefix %s", paramName, v.Prefix)
 	}
 
 	return nil
