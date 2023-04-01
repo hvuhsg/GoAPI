3d970ad0ebfceefbfa01e56e46770346272b56b8
yoyo <hvuhsg5@gmail.com>
2023-04-01 23:49:50 +0300

    add general response

--- responses/response.go
+++ responses/response.go
@@ -9,3 +9,25 @@ type Response interface {
 	ToBytes() []byte
 	StatusCode() int
 }
+
+type response struct {
+	Header  http.Header
+	content []byte
+	code    int
+}
+
+func (r *response) Headers() http.Header {
+	return r.Header
+}
+
+func (r *response) ToBytes() []byte {
+	return r.content
+}
+
+func (r *response) StatusCode() int {
+	return r.code
+}
+
+func NewResponse(content []byte, code int) Response {
+	return &response{content: content, code: code}
+}
